174b63a06ee404b372bc133dda77e820015a1f71
Mateusz-Platek <m.platek1705@gmail.com>
2022-08-25 17:50:47 +0200

    Make calculator work and display solution of mathematical operations

--- javascript.js
+++ javascript.js
@@ -11,6 +11,9 @@ const multiply = function(a, b) {
 }
 
 const divide = function(a, b) {
+    if(b == 0) {
+        return "Don't divide by 0";
+    }
     return a / b;
 }
 
@@ -29,16 +32,75 @@ const operate = function(operator, a, b) {
 const calcScreen = document.querySelector('.screen');
 const digits = document.querySelectorAll('.digit');
 const clear = document.querySelector('.clear');
-let screenValue = 0;
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
+
+let screenValue = null;
+let choosenOperator = 'start';
+let firstNumber = null;
+let secondNumber = null;
+let result = null;
+let next = 'no';
 
 digits.forEach(button => {
     button.addEventListener('click', () => {
-        calcScreen.textContent = calcScreen.textContent + button.textContent;
-        screenValue = Number(calcScreen.textContent);
+        if(calcScreen.textContent == '0' || calcScreen.textContent == '+' || calcScreen.textContent == '-' || calcScreen.textContent == '*' || calcScreen.textContent == '/' || calcScreen.textContent == "Don't divide by 0" || next == 'yes') {
+            calcScreen.textContent = button.textContent;
+            screenValue = Number(calcScreen.textContent);
+            next = 'no';
+        } else {
+            calcScreen.textContent = calcScreen.textContent + button.textContent;
+            screenValue = Number(calcScreen.textContent);
+        }
     })
 });
 
 clear.addEventListener('click', () => {
-    calcScreen.textContent = '';
-    screenValue = 0;
+    calcScreen.textContent = '0';
+    choosenOperator = 'start';
+    firstNumber = null;
+    secondNumber = null;
+    next = 'no';
+});
+
+operators.forEach(button => {
+    button.addEventListener('click', () => {
+        if(choosenOperator != 'start') {
+            next = 'yes';
+            secondNumber = screenValue;
+            result = operate(choosenOperator, firstNumber, secondNumber);
+            screenValue = result;
+            calcScreen.textContent = screenValue;
+            firstNumber = screenValue;
+            if(button.textContent == '+') {
+                choosenOperator = '+';
+            } else if(button.textContent == '-') {
+                choosenOperator = '-';
+            } else if(button.textContent == '*') {
+                choosenOperator = '*';
+            } else if(button.textContent == '/') {
+                choosenOperator = '/';
+            }
+        } else {
+            next = 'yes';
+            firstNumber = screenValue;
+            if(button.textContent == '+') {
+                choosenOperator = '+';
+            } else if(button.textContent == '-') {
+                choosenOperator = '-';
+            } else if(button.textContent == '*') {
+                choosenOperator = '*';
+            } else if(button.textContent == '/') {
+                choosenOperator = '/';
+            }
+        }
+        console.log(firstNumber, secondNumber);
+    });
+});
+
+equals.addEventListener('click', () => {
+    secondNumber = screenValue;
+    result = operate(choosenOperator, firstNumber, secondNumber);
+    screenValue = result;
+    calcScreen.textContent = result;
 });
